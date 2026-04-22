1f076911452627dd618e6f42fdaaf080de3372fe
Guillermo <g.siasp@gmail.com>
2026-04-21 21:05:03 -0400

    index

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const nuevaTarea = document.getElementById("nuevaTarea");
+const agregarTarea = document.querySelector(".agregarTarea");
+const listaTareas = document.querySelector(".listaDeTareas");
+const totalTareas = document.querySelector(".totalTareas");
+const tareasRealizadas = document.querySelector(".tareasRealizadas");
+const listaDeTareas = document.querySelector(".listaDeTareas");
+
+const tareas = ["Revisar correos", "Hacer ejercicio", "Comprar alimentos"];
