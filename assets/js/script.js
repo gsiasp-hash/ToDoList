a18923b13db766cee25fccf5f55047b8888d3bac
Guillermo <g.siasp@gmail.com>
2026-04-23 21:46:32 -0400

    creacion de funciones separadas por accion

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,33 +4,81 @@ const listaTareas = document.querySelector(".listaDeTareas");
 const totalTareas = document.querySelector(".totalTareas");
 const tareasRealizadas = document.querySelector(".tareasRealizadas");
 const listaDeTareas = document.querySelector(".listaDeTareas");
+const eliminarTarea = document.querySelector(".eliminarTarea");
 
-const tareas = ["Revisar correos", "Hacer ejercicio", "Comprar alimentos"];
+const tareas = [
+  { id: 1, tarea: "Hacer la compra", estado: false },
+  { id: 2, tarea: "Lavar el coche", estado: false },
+  { id: 3, tarea: "Pagar las facturas", estado: false },
+];
+
+//creando una nueva tarea
+const crearTarea = (name) => {
+  const ultimatarea = tareas[tareas.length - 1];
+  return {
+    id: ultimatarea ? ultimatarea.id + 1 : 1,
+    tarea: name,
+    estado: false,
+  };
+};
+
+//agregando una nueva tarea al arreglo de tareas
+const addTarea = () => {
+  nombreTarea = nuevaTarea.value.trim();
+  return;
+  const tarea = crearTarea(nombreTarea);
+  tareas.push(tarea);
+  nuevaTarea.value = "";
+  mostrarTareas();
+};
+
+//cambiando el estado de la tarea
+const cambiarEstado = (id) => {
+  const tareaRealizada = tareas.find((tarea) => tarea.id === id);
+  tareaRealizada.estado = !tareaRealizada.estado;
+  mostrarTareas();
+};
+
+//eliminando una tarea, la variable index va a buscar el id de la tarea
+const borrarTarea = (id) => {
+  const index = tareas.findIndex((tarea) => tarea.id === id);
+  tareas.splice(index, 1);
+  if (index === -1) {
+    return;
+  }
+  mostrarTareas();
+};
+
+//contador de tareas realizadas
+const contadorTareasRealizadas = () => {
+  const tareaRealizada = tareas.filter((tarea) => tarea.estado === true);
+  tareasRealizadas.textContent = ` ${tareaRealizada.length}`;
+};
 
 function mostrarTareas() {
   listaDeTareas.innerHTML = "";
-  tareas.forEach((tarea, index) => {
-    const lista = `<li><span class="idTarea">${index + 1} </span> <span class="tarea">${tarea}</span> <input type="checkbox" name="" class="tareaRealizada" id="${index + 1}" /><i class="fa-solid fa-x eliminarTarea" id="${index + 1}"></i></li>`;
+  tareas.forEach((tarea) => {
+    const lista = `<li><span>${tarea.id} </span> <span class=${tarea.estado ? "tareaLista" : ""}>${tarea.tarea}</span> <input type="checkbox" name="" class="tareaRealizada" id="${tarea.id}" ${tarea.estado ? "checked" : ""} /><i class="fa-solid fa-x eliminarTarea"></i></li>`;
     listaDeTareas.innerHTML += lista;
   });
   totalTareas.textContent = ` ${tareas.length}`;
+  contadorTareasRealizadas();
 }
+const tareaRealizada = document.querySelectorAll(".tareaRealizada");
 
-mostrarTareas();
+agregarTarea.addEventListener("click", addTarea);
 
-listaDeTareas.addEventListener("change", () => {
-  const tareasRealizadasCount = document.querySelectorAll(
-    ".tareaRealizada:checked",
-  ).length;
-  tareasRealizadas.textContent = ` ${tareasRealizadasCount}`;
-  tarea.classList.toggle("tareaRealizada", tareaRealizada.checked);
-});
-
-agregarTarea.addEventListener("click", () => {
-  const tarea = nuevaTarea.value;
-  if (tarea) {
-    tareas.push(tarea);
-    nuevaTarea.value = "";
-    mostrarTareas();
+listaDeTareas.addEventListener('change', (e) => {
+  if (e.target.classList.contains('tareaRealizada')) {
+    cambiarEstado(parseInt(e.target.id));
   }
 });
+
+listaDeTareas.addEventListener('click', (e) => {
+  if (e.target.classList.contains('eliminarTarea')) {
+    const id = parseInt(e.target.previousElementSibling.id);
+    borrarTarea(id);
+  }
+});
+
+mostrarTareas();
